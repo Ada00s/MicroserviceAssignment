981ff9110c54962e6e52544011ff0c47a6c0991c
Adrianna <adriannasliwak@gmail.com>
2021-09-13 13:27:09 +0200

    Models

---
+++ ClientApi/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClientApi.Models
+{
+    public abstract class Order
+    {
+        public int CustomerID { get; set; }
+        public IEnumerable<Product> ProductsList { get; set; }
+    }
+    public class CreateOrder : Order
+    {
+
+    }
+    public class ReadOrder : Order
+    {
+        public ShipmentStatus Status { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
