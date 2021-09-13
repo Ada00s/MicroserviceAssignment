a15d21e64843bbbc37f7dcba266cc3ff5297181d
Adrianna <adriannasliwak@gmail.com>
2021-09-13 12:52:12 +0200

    Add project files.

---
+++ MicroserviceAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicroserviceAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
