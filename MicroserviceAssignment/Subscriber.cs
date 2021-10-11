88b9c5f3b17a6d640d93bb457a57eaaaa3ced1b9
Adrianna <adriannasliwak@gmail.com>
2021-10-11 12:31:51 +0200

    Kubernetes deployment

--- MicroserviceAssignment/Subscriber.cs
+++ MicroserviceAssignment/Subscriber.cs
@@ -2,9 +2,6 @@
 using EasyNetQ;
 using MicroserviceAssignment.Handlers;
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace MicroserviceAssignment
